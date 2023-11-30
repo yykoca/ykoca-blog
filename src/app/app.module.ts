813dbe9189315eb8406150258c58e3ee2ba860d3
Yahya Yasir Koca <yykoca.08@gmail.com>
2023-11-30 23:28:21 +0100

    Add layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LayoutModule } from 'src/app/layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
