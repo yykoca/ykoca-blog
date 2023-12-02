3c3b3a5f788b479e44714b97e4500c80e4202ed8
Yahya Yasir Koca <yykoca.08@gmail.com>
2023-12-02 21:38:29 +0100

    Move header component in layout module and add footer component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FeaturedComponent } from 'src/app/layout/featured/featured.component';
+import { FeaturedComponent } from '@app/home/featured/featured.component';
+import { HeaderComponent } from '@layout/header/header.component';
+import { FooterComponent } from '@layout/footer/footer.component';
+import { SharedModule } from '@app/shared';
 
 @NgModule({
-  declarations: [FeaturedComponent],
-  imports: [CommonModule],
-  exports: [FeaturedComponent],
+  declarations: [FeaturedComponent, HeaderComponent, FooterComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [FeaturedComponent, HeaderComponent, FooterComponent],
 })
 export class LayoutModule {}
