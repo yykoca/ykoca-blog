78a7dc0037abd28178bdb2ea20bd4cd3e5a3ce5b
Yahya Yasir Koca <yykoca.08@gmail.com>
2023-12-02 21:39:50 +0100

    Add email subscription component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { EmailSubscriptionComponent } from './email-subscription/email-subscription.component';
 
 const materialModules = [MatToolbarModule, MatIconModule, MatButtonModule];
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [EmailSubscriptionComponent],
   imports: [CommonModule, BrowserAnimationsModule, ...materialModules],
-  exports: [HeaderComponent, ...materialModules],
+  exports: [EmailSubscriptionComponent, ...materialModules],
 })
 export class SharedModule {}
