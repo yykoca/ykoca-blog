98a24eca44c3627399b328a1d003910db5b2f021
Yahya Yasir Koca <yykoca.08@gmail.com>
2023-12-02 21:36:53 +0100

    Add home module and project paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { LayoutModule } from 'src/app/layout/layout.module';
+import { SharedModule } from '@shared/shared.module';
+import { LayoutModule } from '@layout/layout.module';
+import {
+  CommonModule,
+  LocationStrategy,
+  PathLocationStrategy,
+} from '@angular/common';
+import { HomeModule } from '@app/home';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, LayoutModule],
-  providers: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    LayoutModule,
+    HomeModule,
+  ],
+  providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
