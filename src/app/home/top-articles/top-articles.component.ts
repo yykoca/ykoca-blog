029c3589d47f390fc62e00f9c9cd71e05fdcf688
Yahya Yasir Koca <yykoca.08@gmail.com>
2023-12-02 21:43:54 +0100

    Add top articles component

---
+++ src/app/home/top-articles/top-articles.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-articles',
+  templateUrl: './top-articles.component.html',
+  styleUrls: ['./top-articles.component.scss'],
+})
+export class TopArticlesComponent {}
