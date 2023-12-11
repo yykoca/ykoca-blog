be08dbdd2198962ebcc434be97fefcabd27d9535
Yahya Yasir Koca <yykoca.08@gmail.com>
2023-12-11 16:04:28 +0100

    Update article detail component

---
+++ src/app/modules/article/article-detail/article-detail.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import {
+  Article,
+  ArticleService,
+} from '@app/modules/article/article-detail/article.service';
+import { Observable, switchMap } from 'rxjs';
+
+@Component({
+  selector: 'app-article-detail',
+  templateUrl: './article-detail.component.html',
+  styleUrls: ['./article-detail.component.scss'],
+})
+export class ArticleDetailComponent {
+  article$: Observable<{ article: Article }> = this.route.paramMap.pipe(
+    switchMap((params: ParamMap) => {
+      const slug: string | null = params.get('slug');
+      return this.articleService.getArticle(slug);
+    })
+  );
+
+  constructor(
+    private articleService: ArticleService,
+    private route: ActivatedRoute
+  ) {}
+}
