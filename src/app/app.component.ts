4bcff2443b5eeeb95d273b5beb0d89ce2f3245f2
Yahya Yasir Koca <yykoca.08@gmail.com>
2023-12-13 09:28:02 +0100

    Add scroll effect for header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    window.addEventListener('scroll', function () {
+      navbarScroll();
+    });
+    function navbarScroll() {
+      const y = window.scrollY;
+      if (y > 100) {
+        const header = document.getElementsByClassName('header')[0];
+        header.classList.add('small');
+      } else if (y < 100) {
+        const header = document.getElementsByClassName('header')[0];
+        header.classList.remove('small');
+      }
+    }
+  }
+}
