33afa0bf68a3421dca56bdacaa4900449c52f7d0
Yahya Yasir Koca <yykoca.08@gmail.com>
2023-12-18 16:58:47 +0100

    Add tailwind styles to make project responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}'],
+  content: [
+    './src/**/*.{html,ts}',
+    './node_modules/flowbite/**/*.js',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        laptop: "url('/assets/laptop-4840790_1280.jpg')",
+        flower: "url('/assets/flower-4210320_1920.jpg')",
+        desk: "url('/assets/desk-1869579_1920.jpg')",
+        profile: "url('/assets/profile.jpg')",
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 };
