79280e4f7efe996fbf2639cc685d09f40bfe4605
Yahya Yasir Koca <yykoca.08@gmail.com>
2024-01-07 15:46:22 +0100

    Add custom fonts to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,26 @@ module.exports = {
         desk: "url('/assets/desk.jpg')",
         profile: "url('/assets/profile.jpg')",
       }
-    }
+    },
+    fontFamily: {
+      'body': ['"Open Sans"'],
+      'display': ['Oswald'],
+      'amita': ['Amita'],
+      'bree': ['"Bree serif"'],
+      'kings': ['"Kings"'],
+      'sans': ['Exo\\ 2'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'ubuntu': ['"Ubuntu"'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'lightyellow': '#f8ecc6',
+      'graycolor': '#f6f6f4',
+      'darkblue': '#1f1b16'
+    },
   },
   plugins: [
     require('flowbite/plugin')
