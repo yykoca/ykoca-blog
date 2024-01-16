e9defce90ec857ec858946a71563d423d412f516
Yahya Yasir Koca <yykoca.08@gmail.com>
2024-01-16 23:28:08 +0100

    Move featured and top-articles component in to article module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from '@app/modules/home/home.component';
-import { TopArticlesComponent } from './top-articles/top-articles.component';
+import { TopArticlesComponent } from '@app/modules/article/top-articles/top-articles.component';
 import { SharedModule } from '@app/shared';
 import { HomeRoutingModule } from '@app/modules/home/home-routing.module';
 import { CommonModule } from '@angular/common';
-import { FeaturedArticleComponent } from '@app/modules/home/featured-article/featured-article.component';
+import { FeaturedArticleComponent } from '@app/modules/article/featured-article/featured-article.component';
 import { ArticleModule } from '@app/modules/article/article.module';
 
 @NgModule({
