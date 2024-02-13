07cc1701b15a4936997c0eaf5966a46e6ac9d144
Yahya Yasir Koca <yykoca.08@gmail.com>
2024-02-13 18:47:02 +0100

    Fix api url for prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api.yasarkoca.xyz/api',
+  apiUrl: 'https://api.yasarkoca.xyz/api',
   authorId: '018d9a84-7587-705e-92c1-1ae5242713b3',
 };
