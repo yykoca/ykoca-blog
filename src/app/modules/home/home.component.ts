33b80c829e1c0b0341ad9b11d79a45bc4e29a3c0
Yahya Yasir Koca <yykoca.08@gmail.com>
2024-03-25 11:35:54 +0100

    Add some functions to all articles section

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Article } from '@app/modules/article/article.model';
 import { ArticleService } from '@app/modules/article/article.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +10,41 @@ import { Observable } from 'rxjs';
 })
 export class HomeComponent {
   articles$: Observable<Article[]> = this.articleService.getArticles();
+  index = new BehaviorSubject(0);
+  articlePerPage = new BehaviorSubject(1);
 
-  constructor(private articleService: ArticleService) {}
+  screenWidth!: number;
+
+  displayedArticles = combineLatest([
+    this.articleService.getArticles(),
+    this.index,
+    this.articlePerPage,
+  ]).pipe(
+    map(([articles, index, articlePerPage]) =>
+      articles.slice(index, index + articlePerPage)
+    )
+  );
+
+  constructor(private articleService: ArticleService) {
+    this.getScreenSize();
+  }
+
+  changeArticle(value: number) {
+    this.index.next(this.index.value + value);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize() {
+    this.screenWidth = window.innerWidth;
+
+    if (window.innerWidth < 768) {
+      this.articlePerPage.next(1);
+    } else if (window.innerWidth < 1280) {
+      this.articlePerPage.next(2);
+    } else if (window.innerWidth < 1536) {
+      this.articlePerPage.next(3);
+    } else {
+      this.articlePerPage.next(4);
+    }
+  }
 }
